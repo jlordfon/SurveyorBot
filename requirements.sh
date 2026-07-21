e3a88322bed9b67e5dacea654da00b60e44fcf50
jlordfon <jlordfon@alumni.cmu.edu>
2026-07-21 01:39:53 -0400

    [buildEnvironment/SettingUpTesting] Adjusting yml

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
 apt-get update
 apt-get install -y build-essential
-apt install -y cmake ninja-build clang clang-format clang-tidy
+apt install -y cmake=4.2.3
+apt install -y ninja-build clang clang-format clang-tidy
 
 # VCPkg
 apt-get -y install curl zip unzip tar pkg-config
